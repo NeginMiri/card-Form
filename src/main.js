11ab4f81d29d8a8a36366547222b7f709878fbc5
NeginMiri <Negimiri09@gmail.com>
2026-02-21 20:24:21 +0330

    v2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import './style.css'
 
+const formeEl = document.getElementById("form");
+const cartnumberEl = document.getElementById ("cartnumber");
+const expiryEl = document.getElementById("expiry");
+const cvvEl = document.getElementById("cvv");
+const nameEl = document.getElementById("name");
+const pay_btnEl = document.getElementById("pay-btn");
+const successModalEl = document.getElementById("successModal");
+const close_btnEl = document.getElementById("close-btn");
+
